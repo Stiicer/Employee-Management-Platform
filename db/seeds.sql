49b7cb30c1a8be6a059ee23e1d6026e72cc4cd0e
Stiicer <diegoandremora@gmail.com>
2022-11-16 17:47:52 -0500

    added schema seeds and some code to server

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+-- seeds the database
+INSERT INTO department
+VALUES ('Sales'),('Engineering'),('Finance'),('Legal');
+
+INSERT INTO role (title,salary,department_id)
+VALUES ('sales lead',100000,1),('sales person', 80000,1),
+('lead engineer',200000,2),('software engineer',150000,2),
+('account manager',160000,3),('accountant',125000,3),
+('legal lead',300000,4),('lawyer',200000,4);
+
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES('Troy', 'Doe',1,NULL),('Hunter','Chan',2,1),
+('Eric','Brat',3,NULL),('Diego','Champ',4,3),
+('Connor','Lol',5,NULL),('Big','Nate',6,5),
+('Charles','Barkley',7,NULL),('Lebron','James',8,7);
